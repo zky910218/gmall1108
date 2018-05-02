df975399c07d029edc1e6f95c86b163c926ad42e
bingheng <zky910218@163.com>
2018-05-02 18:34:41 +0800

    Mapper接口

---
+++ gmall-usermange/src/main/java/cn/nosnow/gmall/usermanage/mapper/UserAddressMapper.java
@@ -0,0 +1,12 @@
+package cn.nosnow.gmall.usermanage.mapper;
+
+import cn.nosnow.gmall.bean.UserAddress;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * Created by Think on 2018/5/1.
+ * @author bingHeng
+ * @date 2018.05.01
+ */
+public interface UserAddressMapper extends Mapper<UserAddress> {
+}
