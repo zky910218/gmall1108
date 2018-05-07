7b4e34d0a6e63f3b58aa105aecbe44454633e23c
bingheng <zky910218@163.com>
2018-05-07 19:14:26 +0800

    实体类

---
+++ gmall-order-web/src/main/java/cn/nosnow/gmall1108/order/GmallOrderWebApplication.java
@@ -0,0 +1,12 @@
+package cn.nosnow.gmall1108.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmallOrderWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmallOrderWebApplication.class, args);
+	}
+}
--- gmall-order-web/src/main/java/cn/nosnow/gmall1108/order/controller/OrderController.java
+++ gmall-order-web/src/main/java/cn/nosnow/gmall1108/order/controller/OrderController.java
@@ -4,6 +4,7 @@ import cn.nosnow.gmall.bean.UserAddress;
 import cn.nosnow.gmall.service.UserService;
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.alibaba.fastjson.JSON;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
