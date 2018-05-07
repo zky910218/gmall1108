63fc613b517935979aaab4a9b8ff288638daae12
bingheng <zky910218@163.com>
2018-05-07 19:23:47 +0800

    order的web应用模块

---
+++ gmall-order-web/src/test/java/cn/nosnow/gmall1108/order/GmallOrderWebApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.nosnow.gmall1108.order;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GmallOrderWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
