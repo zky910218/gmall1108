fdb02ba4e9843d08e690ba1dc32c821d99dae6a7
bingheng <zky910218@163.com>
2018-05-14 12:42:52 +0800

    保存数据controller层

--- gmall-manage-web/src/main/java/cn/nosnow/gmall1108/manage/controller/ManageController.java
+++ gmall-manage-web/src/main/java/cn/nosnow/gmall1108/manage/controller/ManageController.java
@@ -1,7 +1,12 @@
 package cn.nosnow.gmall1108.manage.controller;
 
+import cn.nosnow.gmall.bean.BaseAttrInfo;
+import cn.nosnow.gmall.service.ManageService;
+import com.alibaba.dubbo.config.annotation.Reference;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by Think on 2018/5/4.
@@ -11,9 +16,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ManageController {
 
+    @Reference
+    private ManageService manageService;
+
     @RequestMapping("index")
     public String index() {
-
         return "index";
     }
 
@@ -21,4 +28,12 @@ public class ManageController {
     public String attrListPage() {
         return "attrListPage";
     }
+
+    @RequestMapping(value = "saveAttrInfo",method = RequestMethod.POST)
+    @ResponseBody
+    public String saveAttrInfo(BaseAttrInfo baseAttrInfo){
+        manageService.saveAttrInfo(baseAttrInfo);
+        return "success";
+    }
+
 }
