48fba5ceae7ac95b3ce7c60078ebd915f405e09f
bingheng <zky910218@163.com>
2018-05-14 12:40:53 +0800

    保存数据serviceImpl方法saveAttrInfo

--- gmall-manage-service/src/main/java/cn/nosnow/gmall1108/manage/service/impl/ManageServiceImpl.java
+++ gmall-manage-service/src/main/java/cn/nosnow/gmall1108/manage/service/impl/ManageServiceImpl.java
@@ -1,9 +1,6 @@
 package cn.nosnow.gmall1108.manage.service.impl;
 
-import cn.nosnow.gmall.bean.BaseAttrInfo;
-import cn.nosnow.gmall.bean.BaseCatalog1;
-import cn.nosnow.gmall.bean.BaseCatalog2;
-import cn.nosnow.gmall.bean.BaseCatalog3;
+import cn.nosnow.gmall.bean.*;
 import cn.nosnow.gmall.service.ManageService;
 import cn.nosnow.gmall1108.manage.mapper.*;
 import com.alibaba.dubbo.config.annotation.Service;
@@ -31,6 +28,9 @@ public class ManageServiceImpl implements ManageService {
     @Autowired
     private BaseAttrInfoMapper baseAttrInfoMapper;
 
+    @Autowired
+    private BaseAttrValueMapper baseAttrValueMapper;
+
     @Override
     public List<BaseCatalog1> getCatalog1() {
         return baseCatalog1Mapper.selectAll();
@@ -55,5 +55,35 @@ public class ManageServiceImpl implements ManageService {
         BaseAttrInfo baseAttrInfo = new BaseAttrInfo();
         baseAttrInfo.setCatalog3Id(catalog3Id);
         return baseAttrInfoMapper.select(baseAttrInfo);
+    }
+
+    @Override
+    public void saveAttrInfo(BaseAttrInfo baseAttrInfo) {
+        // 如果有主键就进行更新，没有则进行插入
+        if(baseAttrInfo.getId() != null && baseAttrInfo.getId().length() > 0) {
+            baseAttrInfoMapper.updateByPrimaryKey(baseAttrInfo);
+        } else {
+            // 防止主键被赋值为空字符串
+            if (baseAttrInfo.getId().length() == 0) {
+                baseAttrInfo.setId(null);
+            }
+            baseAttrInfoMapper.insertSelective(baseAttrInfo);
+        }
+        // 将原来的属性值全部清空
+        BaseAttrValue baseAttrValue4Del = new BaseAttrValue();
+        baseAttrValue4Del.setAttrId(baseAttrInfo.getId());
+        baseAttrValueMapper.delete(baseAttrValue4Del);
+
+        // 重新插入属性值
+        if(baseAttrInfo.getAttrValueList() != null && baseAttrInfo.getAttrValueList().size() > 0) {
+            for(BaseAttrValue attrValue : baseAttrInfo.getAttrValueList()) {
+                // 防止主键被赋值上一个空字符串
+                if(attrValue.getId()!= null && attrValue.getId().length()== 0){
+                    attrValue.setId(null);
+                }
+                attrValue.setAttrId(baseAttrInfo.getId());
+                baseAttrValueMapper.insertSelective(attrValue);
+            }
+        }
     }
 }
